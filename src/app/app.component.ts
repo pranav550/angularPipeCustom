b630af03431c292be26d87640776dcdf4a885043
pranav <pranavverma550@gmail.com>
2019-04-21 08:24:59 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularcustomPipe';
+  employees:any=[
+    {code :'001', name:'Ajeet', gender:'male', salary:50000 },
+    {code :'002', name:'Abhay', gender:'male', salary:50000},
+    {code :'003', name:'Akash', gender:'male', salary:50000},
+    {code :'004', name:'Ravi', gender:'male', salary:50000},
+    {code :'005', name:'Rani', gender:'female', salary:50000},
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MypipePipe } from './mypipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MypipePipe
   ],
   imports: [
     BrowserModule,
